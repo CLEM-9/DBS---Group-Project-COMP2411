7339367e11e115da5e6db37de65ba0a9fbbf90c7
CLEM-9 <tommaso.fcostantini@gmail.com>
2024-11-06 13:57:51 +0800

    Create_Tables

---
+++ source/main.py
@@ -0,0 +1,29 @@
+import sys
+import mysql.connector
+
+from source.connection import Connection
+
+if __name__ == '__main__':
+    database = input('Enter your database name: ')
+    user = input('Enter your username: ')
+    password = input('Enter your password: ')
+    accessStatus = True
+    try:
+        access1 = Connection(user, password, database)
+    except mysql.connector.Error as err:
+        print(f"ERROR CODE: {err}")
+        accessStatus = False
+    finally:
+        if not accessStatus:
+            print("Connection Failed")
+            sys.exit()
+        # noinspection PyUnboundLocalVariable
+        if access1.connection.is_connected():
+            print(f"Connected to {database}")
+            while True:
+                closeConnection = input("Close the connection? (y/n): ")
+                if closeConnection == 'y':
+                    access1.connection.close()
+                    print(f"Disconnected from {database}")
+                    sys.exit()
+
