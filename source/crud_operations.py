6477ecde7fba374c510e70e55eb9ad7a82d0a3d4
CLEM-9 <tommaso.fcostantini@gmail.com>
2024-11-12 14:06:54 +0800

    CRUD

--- source/crud_operations.py
+++ source/crud_operations.py
@@ -1,108 +1,119 @@
 import mysql.connector as mysql
 from mysql.connector import Error
 
-class RegisteredUsers:
+class Database:
     def __init__(self, cursor, connection):
         self.cursor = cursor
         self.connection = connection
 
+    # checks if logging_in user is attendee or administrator
+    def check_email(self, connection, email, password):
+        attendee = 2
+        admin = 1
+        try:
+            self.cursor.execute(
+                "SELECT * FROM attendees WHERE email = %s AND password = %s",
+                (email, password)
+            )
+            if_attendee = self.cursor.fetchone()
+            self.cursor.execute(
+                "SELECT * FROM Administrators WHERE adminEmail = %s AND adminPassword = %s",
+                (email, password)
+            )
+            if_admin = self.cursor.fetchone()
+            if if_attendee:
+                print("Login successful!")
+                return attendee
+            elif if_admin:
+                print("Login successful!")
+                return admin
+            else:
+                print("Login failed. Please try again.")
+                return 0
+
+        except Error as e:
+            print(f"Error code: {e}")
+
+        finally:
+            self.cursor.close()
+            connection.close()
+
+    def drop_database(self):
+        try:
+            self.cursor.execute("DROP DATABASE IF EXISTS banquet_database")
+        except Error as e:
+            print(f"Error: {e}")
+        finally:
+            self.cursor.close()
+            self.connection.close()
+
+class Attendees(Database):
+    def __init__(self):
+        super().__init__(self.cursor, self.connection)
+
     # creates users
     def create(self, email, password, address, lastName, firstName, phone, attendeeType, affiliateOrganization):
-        sql = "INSERT INTO RegisteredUsers(email, password, address, lastName, firstName, phone, attendeeType, affiliateOrganization) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO attendees(email, password, address, lastName, firstName, phone, attendeeType, affiliateOrganization) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
         values = (email, password, address, lastName, firstName, phone, attendeeType, affiliateOrganization)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
-        return f"{firstName} {lastName} added successfully."
+        try:
+            self.cursor.execute(sql, values)
+            self.connection.commit()
+            return f"{firstName} {lastName} added successfully."
+        except Error as e:
+            return f"Could not add user\nError code: {e}"
 
     # returns an array of arrays containing all information available
     def read(self):
-        sql = "SELECT * FROM RegisteredUsers"
+        sql = "SELECT * FROM attendees"
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
         return result
 
     # if the information is not provided by the user, pass None as argument
     def update(self, email, password, address, phone, attendeeType, affiliateOrganization):
-        sql = "UPDATE RegisteredUsers SET "
+        sql = "UPDATE attendees SET "
+        values = []
+        message = "Following fields are updated:\n"
 
         # appends all the information fields that need to be updated
         if email is not None:
             sql = sql + "email = %s, "
+            values.append(email)
+            message += f"email = {email}\n"
         if password is not None:
             sql = sql + "password = %s, "
+            message += f"password = {password}\n"
+            values.append(password)
         if address is not None:
             sql = sql + "address = %s, "
+            values.append(address)
+            message += f"address = {address}\n"
         if phone is not None:
             sql = sql + "phone = %s, "
+            values.append(phone)
+            message += f"phone = {phone}\n"
         if attendeeType is not None:
             sql = sql + "attendeeType = %s, "
+            values.append(attendeeType)
+            message += f"attendeeType = {attendeeType}\n"
         if affiliateOrganization is not None:
             sql = sql + "affiliateOrganization = %s, "
+            values.append(affiliateOrganization)
+            message += f"affiliateOrganization = {affiliateOrganization}\n"
+
+        # removes the last ", "
+        sql = sql[:-2]
+        sql = sql + " WHERE email = %s"
+        self.cursor.execute(sql, values)
+        self.connection.commit()
+        return message
 
     # deletes user with the primary key "email"
     def delete(self, email):
-        sql = "DELETE FROM RegisteredUsers WHERE email = %s"
+        sql = "DELETE FROM attendees WHERE email = %s"
         values = email
         self.cursor.execute(sql, values)
         self.connection.commit()
         return "User deleted successfully."
 
-def register(cursor, connection,email, password, address, last_name, first_name, phone, attendee_type, affiliate_organization):
-    try:
-        cursor.execute(
-            "INSERT INTO attendees (email, password, address, lastName, firstName, phone, attendeeType, affiliateOrganization) "
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-            (email, password, address, last_name, first_name, phone, attendee_type, affiliate_organization)
-        )
-        connection.commit()
-        print("Registration successful!")
-        print("Loggin in...")
-    except Error as e:
-        print(f"Error: {e}")
 
-def check_email(cursor, connection, email, password):
-    try:
-        attendee = 2
-        admin = 1
-        cursor.execute(
-            "SELECT * FROM attendees WHERE email = %s AND password = %s",
-            (email, password)
-        )
-        if_attendee = cursor.fetchone()
-        cursor.execute(
-            "SELECT * FROM Administrators WHERE adminEmail = %s AND adminPassword = %s",
-            (email, password)
-        )
-        if_admin = cursor.fetchone()
-        if if_attendee:
-            print("Login successful!")
-            return attendee
-        elif if_admin:
-            print("Login successful!")
-            return admin
-        else:
-            print("Login failed. Please try again.")
-            return 0
-
-    except Error as e:
-        print(f"Error: {e}")
-
-    finally:
-        cursor.close()
-        connection.close()
-
-def drop_database(connection, cursor):
-    try:
-        cursor.execute("DROP DATABASE IF EXISTS banquet_database")
-    except Error as e:
-        print(f"Error: {e}")
-    finally:
-        cursor.close()
-        connection.close()
-
-def create_user(name, email):
-    sql = "INSERT INTO users (name, email) VALUES (%s, %s)"
-    values = (name, email)
-    cursor.execute(sql, values)
-    connection.commit()
-    print(f"User {name} added successfully.")
